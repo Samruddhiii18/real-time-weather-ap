bba64a25650861fb2e857ff12be4a347cc831fb0
Samruddhi Bichpuriya <166840342+Samruddhiii18@users.noreply.github.com>
2026-04-15 00:17:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded' , () => {
+    const cityInput = document.getElementById("city-input");
+    const getWeatherBtn = document.getElementById("get-weather-btn");
+    const weatherInfo = document.getElementById("weather-info");
+    const cityNameDisplay = document.getElementById("city-name");
+    const temperatureDisplay = document.getElementById("temperature");
+    const descriptionDisplay = document.getElementById("description");
+    const errorMessage = document.getElementById("error-message");
+    const api_key = "76489b91a6035bd04c4d92d1e475c9a8"
+
+    getWeatherBtn.addEventListener('click' , async () =>{
+        const city = cityInput.value.trim()
+        if(!city) return;
+
+        //Server may throw n error
+        // server/database is always in another continent
+
+        try {
+           const weatherData = await fetchWeatherData(city);
+           displayWeatherData(weatherData);
+        } catch (error) {
+            showError()
+        }
+    })
+    async function fetchWeatherData(city){
+        //get the data
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api_key}`
+        const response = await fetch(url);
+        if(!response.ok){
+            throw new Error("City not found!!");
+        }
+        const data = await response.json()
+        return data;
+    }
+
+    function displayWeatherData(data){
+        //display 
+        const {name, main, weather} = data;
+        cityNameDisplay.textContent = name;
+        temperatureDisplay.textContent = `Temperature : ${main.temp}°C`;
+        descriptionDisplay.textContent = `Weather : ${weather[0].description}`
+
+        weatherInfo.classList.remove('hidden');
+        errorMessage.classList.add('hidden');
+    }
+
+    function showError(){
+        weatherInfo.classList.add('hidden');
+        errorMessage.classList.remove('hidden');
+    }
+});
